03fb3df4b1884a73fa5ada9b83fb2d692f9ee6d8
xamfy <19357995+xamfy@users.noreply.github.com>
2019-06-05 05:00:27 +0530

    Add code for local_auth and platform integration

--- android/app/src/main/java/com/example/flutter_platform/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_platform/MainActivity.java
@@ -2,12 +2,62 @@ package com.example.flutter_platform;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
+import io.flutter.app.FlutterFragmentActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class MainActivity extends FlutterActivity {
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+
+public class MainActivity extends FlutterFragmentActivity {
+  private static final String CHANNEl = "samples.flutter.dev/battery";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    // new MethodChannel(getFlutterView(), CHANNEl).setMethodCallHandler(new
+    // MethodCallHandler() {
+    // @Override
+    // public void onMethodCall(MethodCall call, Result result) {
+    // if (call.method.equals("getBatteryLevel")) {
+    // int batteryLevel = getBatteryLevel();
+    // if (batteryLevel != -1) {
+    // result.success(batteryLevel);
+    // } else {
+    // result.error("UNAVAILABLE", "Battery level not available.", null);
+    // }
+    // } else {
+    // result.notImplemented();
+    // }
+    // }
+    // });
   }
+
+  // private int getBatteryLevel() {
+  // int batteryLevel = -1;
+  // if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+  // BatteryManager batteryManager = (BatteryManager)
+  // getSystemService(BATTERY_SERVICE);
+  // batteryLevel =
+  // batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+  // } else {
+  // Intent intent = new
+  // ContextWrapper(getApplicationContext()).registerReceiver(null,
+  // new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+  // batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100)
+  // / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+  // }
+  // return batteryLevel;
+  // }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+void main() => runApp(MyHomePage());
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,68 +15,132 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // The client and host sides of a channel are connected through a
+  // channel name passed in the channel constructor.
+  // All channel names used in a single app must be unique;
+  // prefix the channel name with a unique ‘domain prefix’,
+  // for example: samples.flutter.dev/battery.
 
-  void _incrementCounter() {
+  final LocalAuthentication auth = LocalAuthentication();
+  bool _canCheckBiometrics;
+  List<BiometricType> _availableBiometrics;
+  String _authorized = 'Not Authorized';
+
+  Future<void> _checkBiometrics() async {
+    bool canCheckBiometrics;
+    try {
+      canCheckBiometrics = await auth.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    if (!mounted) return;
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _canCheckBiometrics = canCheckBiometrics;
+    });
+  }
+
+  Future<void> _getAvailableBiometrics() async {
+    List<BiometricType> availableBiometrics;
+    try {
+      availableBiometrics = await auth.getAvailableBiometrics();
+    } on PlatformException catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _availableBiometrics = availableBiometrics;
+    });
+  }
+
+  Future<void> _authenticate() async {
+    bool authenticated = false;
+    try {
+      authenticated = await auth.authenticateWithBiometrics(
+          localizedReason: 'Scan you fingerprint to authenticate',
+          useErrorDialogs: true,
+          stickyAuth: true);
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _authorized = authenticated ? 'Authorized' : 'Not Authorized';
+    });
+  }
+
+  static const platform = const MethodChannel('samples.flutter.dev/battery');
+
+  // Get battery level
+  String _batteryLevel = 'Unknown battery level';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    return MaterialApp(
+        home: Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Plugin example app'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: ConstrainedBox(
+        constraints: const BoxConstraints.expand(),
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Text('Can check biometrics: $_canCheckBiometrics\n'),
+            RaisedButton(
+              child: const Text('Check biometrics'),
+              onPressed: _checkBiometrics,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Text('Available biometrics: $_availableBiometrics\n'),
+            RaisedButton(
+              child: const Text('Get available biometrics'),
+              onPressed: _getAvailableBiometrics,
             ),
+            Text('Current State: $_authorized\n'),
+            RaisedButton(
+              child: const Text('Authenticate'),
+              onPressed: _authenticate,
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
+    // Platform integration example
+
+    // ################
+
+    // return Material(
+    //   child: Center(
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //       children: [
+    //         RaisedButton(
+    //           child: Text('Get Battery Level'),
+    //           onPressed: _getBatteryLevel,
+    //         ),
+    //         Text(_batteryLevel),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
+
